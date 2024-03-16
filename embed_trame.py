6f7fc4603bed309a0d5f5801b690a2e7ac530927
edsaa <esaavedrac@u.northwestern.edu>
2024-03-16 16:09:34 -0500

    cloudfared

--- embed_trame.py
+++ embed_trame.py
@@ -1,60 +1,58 @@
 import streamlit as st
 import shlex
-from subprocess import Popen, check_output
+from subprocess import Popen, PIPE
 import atexit
-import socket
+import os
+from pathlib import Path
 
-def show_config():
-    command = "/home/adminuser/venv/bin/streamlit config show"
-    args = shlex.split(command)
-    output = check_output(args, text=True)
-    return output
+CLOUDFARE_PATH = Path("./cloudflared-linux-amd64")
+if not CLOUDFARE_PATH.exists():
+    os.system("wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64")
+    os.system("chmod +x cloudflared-linux-amd64")
 
-def get_server_ip():
-
-    s = socket.socket(
-        family = socket.AF_INET,    # (host, port)
-        type = socket.SOCK_DGRAM )
-    
-    s.connect(('8.8.8.8', 1))
-    ip, port = s.getsockname()
-
-    print(f"{ip =}, {port =}")
-    s.close()
-    return ip, port
-
-def launch_trame(path_script: str, ip:str):
-    command = f"""/home/adminuser/venv/bin/python {path_script} --port 12345"""
+def run_cloudflared():
+    command = "nohup ./cloudflared-linux-amd64 tunnel --url http://localhost:12345 &"
     args = shlex.split(command)
     p = Popen(args)
-
     return p
 
+def launch_trame(path_script: str):
+    command = f"""/home/adminuser/venv/bin/python {path_script} --port 12345"""
+    args = shlex.split(command)
+    p = Popen(args, stdout=PIPE)
+    return p
 
-def close_trame(p: Popen):
+def close_trame(trame_proc: Popen):
     print("Closing trame")
-    p.terminate()
+    trame_proc.terminate()
 
+def get_address():
+    command = """grep -o 'https://.*\.trycloudflare.com' nohup.out | head -n 1 | xargs -I {} echo "{}" > address.txt"""
+    os.system(command)
+    with open("address.txt") as f:
+        address = f.read()
+
+    return address
 
 def main():
-    st.code(show_config())
+    # st.code(show_config())
     st.title("Trame within streamlit")
-    ip, _ = get_server_ip()
-    p = launch_trame("trame_example/solution_cone.py", ip)
+    
+    p = launch_trame("trame_example/solution_cone.py")
     atexit.register(close_trame, p)
+        
+    if "cloudfared_running" not in st.session_state:
+        run_cloudflared()
+        st.session_state.cloudfared_running = True
+    
 
-    st.components.v1.iframe("http://localhost:12345", height=400)
-    "*****"
-    st.components.v1.html(
-        """
-         <iframe src="http://localhost:12345" title="description"></iframe> 
-        """
-    )
-    # trame_component()
+    adresss = get_address()
+    st.write(adresss)
+    st.components.v1.iframe(adresss, height=400)
 
-    if st.button("Reset trame"):
-        p.terminate()
-        st.rerun()
+    # if st.button("Reset trame"):
+    #     p.terminate()
+    #     st.rerun()
 
 if __name__ == "__main__":
     main()
