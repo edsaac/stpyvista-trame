088e2b992006564e6663c16a866042912391cb36
edsaa <esaavedrac@u.northwestern.edu>
2024-03-16 10:57:21 -0500

    get ip

--- embed_trame.py
+++ embed_trame.py
@@ -2,12 +2,21 @@ import streamlit as st
 import shlex
 from subprocess import Popen
 import atexit
-
+import streamlit.components.v1 as components
+import socket
 
 def launch_trame(path_script: str):
+    
+    global trame_component
+    
     command = f"python {path_script} --port 1234"
     args = shlex.split(command)
     p = Popen(args)
+
+    trame_component = components.declare_component(
+        "trame_component",
+        url="http://localhost:1234"
+    )
     return p
 
 
@@ -20,11 +29,26 @@ def main():
 
     st.title("Trame within streamlit")
 
+    "### Python `socket` (?)"
+with st.echo():
+
+    s = socket.socket(
+        family = socket.AF_INET,    # (host, port)
+        type = socket.SOCK_DGRAM )
+    
+    s.connect(('8.8.8.8', 1))
+    ip, port = s.getsockname()
+
+    print(f"{ip =}, {port =}")
+    st.metric("**IP**", ip)
+    st.metric("**Port**", port)
+    s.close()
+
     p = launch_trame("trame_example/solution_cone.py")
     atexit.register(close_trame, p)
 
-    st.components.v1.iframe("http://localhost:1234", height=400)
-
+    # st.components.v1.iframe("http://localhost:1234", height=400)
+    trame_component()
 
 if __name__ == "__main__":
     main()
