fb584384c60b22b8ce0d28430159e557cf57f1eb
edsaa <esaavedrac@u.northwestern.edu>
2024-03-18 08:58:49 -0500

    better handler

--- embed_trame.py
+++ embed_trame.py
@@ -5,50 +5,66 @@ import atexit
 import os
 from pathlib import Path
 
-CLOUDFARE_PATH = Path("./cloudflared-linux-amd64")
-if not CLOUDFARE_PATH.exists():
-    os.system("wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64")
-    os.system("chmod +x cloudflared-linux-amd64")
+IN_COMMUNITY_CLOUD = False
 
-def run_cloudflared():
-    command = "./cloudflared-linux-amd64 tunnel --url http://localhost:12345"
+CLOUDFARED_PATH = Path("./cloudflared/cloudflared-linux-amd64")
+PYCOMMAND = "/home/adminuser/venv/bin/python" if IN_COMMUNITY_CLOUD else "python"
+
+if not Path("./cloudflared").exists():
+    os.system("mkdir cloudflared")
+    st.rerun()
+
+if not CLOUDFARED_PATH.exists():
+    os.system(f"wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O {CLOUDFARED_PATH}")
+    os.system(f"chmod 777 {CLOUDFARED_PATH}")
+    st.rerun()
+
+@st.cache_resource
+def launch_cloudflared(dummy:str = "cloud"):
+    command = "./cloudflared/cloudflared-linux-amd64 tunnel --url http://localhost:12345"
     args = shlex.split(command)
     p = Popen(args, stdout=PIPE, stderr=PIPE, text=True)
     
     address = "not_found"
     
     for _ in range(50):
-        line = p.stdout.readline()
+        line = p.stderr.readline()
+        print(line)
         if "https" in line and "trycloudflare.com" in line:
             address = line.rpartition("INF")[-1].replace("|","").strip()
             break
         
-    return address
+    return p, address
 
 def launch_trame(path_script: str):
-    command = f"""/home/adminuser/venv/bin/python {path_script} --port 12345"""
+    command = f"""{PYCOMMAND} {path_script} --port 12345"""
     args = shlex.split(command)
-    p = Popen(args, stdout=PIPE)
+    p = Popen(args, stdout=PIPE, stderr=PIPE, text=True)
     return p
 
-def close_trame(trame_proc: Popen):
+def close_trame(p_trame: Popen):
     print("Closing trame")
-    trame_proc.terminate()
+    p_trame.terminate()
+    del st.session_state.trame_running
 
-
+def close_cloudflared(p_cloudflared: Popen):
+    print("Closing cloudflared")
+    p_cloudflared.terminate()
+    del st.session_state.cloudfared_running
 
 def main():
-    # st.code(show_config())
     st.title("Trame within streamlit")
     
-    p = launch_trame("trame_example/solution_cone.py")
-    atexit.register(close_trame, p)
-        
     if "cloudfared_running" not in st.session_state:
-        address = run_cloudflared()
-        st.session_state.cloudfared_running = True
-    
+        p_cloudflared, address = launch_cloudflared()
+        atexit.register(close_cloudflared, p_cloudflared)
+        st.session_state.cloudfared_running = p_cloudflared
 
+    if "trame_running" not in st.session_state:
+        p_trame = launch_trame("trame_example/solution_cone.py")
+        atexit.register(close_trame, p_trame)
+        st.session_state.trame_running = p_trame
+    
     st.write(address)
     st.components.v1.iframe(address, height=400)
 
