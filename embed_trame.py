be43d5734af7dad54a282cadb11ed72fc070cb7e
edsaa <esaavedrac@u.northwestern.edu>
2024-04-03 15:21:49 -0500

    Less nice

--- embed_trame.py
+++ embed_trame.py
@@ -1,138 +1,87 @@
-import streamlit as st
-import shlex
-from subprocess import Popen, check_output
-from subprocess import PIPE, STDOUT
-import atexit
 import os
+import streamlit as st
 from pathlib import Path
-from collections import namedtuple
 from time import sleep
 
-CF_Connection = namedtuple("CF_Connection", ["process", "address"])
-PORT = 17007
-
-IN_COMMUNITY_CLOUD = True
-PYCOMMAND = "/home/adminuser/venv/bin/python" if IN_COMMUNITY_CLOUD else "python"
-CONTROL_PANEL = True
-
+PORT = 12346
 CLOUDFLARED_PATH = Path("./cloudflared/cloudflared-linux-amd64")
-TRAME_APP_PATH = "./trame_example/grand_canyon.py"
 
-st.set_page_config(initial_sidebar_state="collapsed")
+in_community_cloud = True
+python_executable = "/home/adminuser/venv/bin/python" if in_community_cloud else "python"
 
-if not Path("./cloudflared").exists():
-    os.system("mkdir cloudflared")
-    st.rerun()
+def download_cloudflared():
+    if not Path("./cloudflared").exists():
+        os.system("mkdir cloudflared")
+        st.rerun()
 
-if not CLOUDFLARED_PATH.exists():
-    os.system(
-        f"wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O {CLOUDFLARED_PATH}"
-    )
-    os.system(f"chmod 777 {CLOUDFLARED_PATH}")
-    st.rerun()
+    if not CLOUDFLARED_PATH.exists():
+        os.system(
+            f"wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O {CLOUDFLARED_PATH}"
+        )
+        os.system(f"chmod 777 {CLOUDFLARED_PATH}")
+        st.rerun()
 
+    return True
+
+@st.cache_resource
+def launch_trame(path_script: str):
+    command = f"""{python_executable} -u {path_script} --server --port {PORT} &"""
+    os.system(command)
 
 @st.cache_resource
 def launch_cloudflared(dummy: str = "cloud"):
     command = (
-        f"./cloudflared/cloudflared-linux-amd64 tunnel --url http://localhost:{PORT}"
+        f"./cloudflared/cloudflared-linux-amd64 tunnel --url http://localhost:{PORT} > ./cloudflared/log.txt 2>&1 &"
     )
-    args = shlex.split(command)
-    p = Popen(args, stdout=PIPE, stderr=PIPE, text=True)
+    os.system(command)
 
-    address = "not_found"
-    sleep(2)
+    sleep(5)
 
-    for _ in range(50):
-        line = p.stderr.readline()
-        print(line)
-        if "https" in line and ".trycloudflare.com" in line:
-            address = line.rpartition("INF")[-1].replace("|", "").strip()
-            break
+    with open("./cloudflared/log.txt") as f:
+        for line in f:
+            print(line)
+            if "https" in line and ".trycloudflare.com" in line:
+                address = line.rpartition("INF")[-1].replace("|", "").strip()
+                break
 
-    return CF_Connection(p, address)
-
+    return address
 
 @st.cache_resource
-def launch_trame(path_script: str):
-    command = f"""{PYCOMMAND} -u {path_script} --server --port {PORT} &"""
-    # os.system(command)
-    args = shlex.split(command)
-    p = Popen(args, stdout=PIPE, stderr=PIPE, text=True)
+def initialize_server(dummy:str = "cloud"):
+    try:
+        with open("sentinel.txt") as f:
+            address = f.readline()
 
-    return p
-
-
-def close_all(p_trame: Popen, p_cloudflared: Popen):
-    close_trame(p_trame)
-    close_cloudflared(p_cloudflared)
-    st.rerun()
-
-
-def close_trame(p_trame: Popen):
-    print("Closing trame")
-    p_trame.terminate()
-    del st.session_state.trame_running
-
-
-def close_cloudflared(p_cloudflared: Popen):
-    print("Closing cloudflared")
-    p_cloudflared.terminate()
-    del st.session_state.cloudflared
-
+    except FileNotFoundError:
+        download_cloudflared()
+        launch_trame("trame_example/grand_canyon.py")
+        address = launch_cloudflared()
+        
+        with open("sentinel.txt", "w") as f:
+            f.write(address)
+    
+    return address
 
 def main():
+    #--------------------------------
+    # App
+    #--------------------------------
+    
     st.markdown(
         """
-    <style>
+        <style>
         div[data-testid="stAppViewBlockContainer"]{
             padding-top: 1rem;
         }
-    </style>
-    """,
+        </style>
+        """,
         unsafe_allow_html=True,
     )
 
     st.title("🧇 Trame within streamlit")
-
-    if "trame_running" not in st.session_state:
-        p_trame = launch_trame(TRAME_APP_PATH)
-        atexit.register(close_trame, p_trame)
-        st.session_state.trame_running = p_trame
-
-    if "cloudflared" not in st.session_state:
-        cloudflared = launch_cloudflared()
-        atexit.register(close_cloudflared, cloudflared.process)
-        st.session_state.cloudflared = cloudflared
-        st.rerun()
-
-    address = st.session_state.cloudflared.address
+    address = initialize_server()
     st.components.v1.iframe(address, height=580)
+
     
-
-    if CONTROL_PANEL:
-        with st.sidebar:
-            if (
-                st.text_input("Controls", type="password", label_visibility="collapsed")
-                == st.secrets.control_panel
-            ):
-                st.info(f"Running on {address}")
-
-                if st.button("Reset cloudflared", on_click=st.cache_resource.clear):
-                    close_all(
-                        st.session_state.trame_running, st.session_state.cloudflared.process
-                    )
-
-                with st.form("Run commands"):
-                    quick_command = st.text_area("Command")
-                    run_command = st.form_submit_button("Run")
-
-                if run_command:
-                    task = check_output(
-                        ["bash", "-c", f"\"{quick_command}\""], text=True, stderr=STDOUT
-                    )
-                    st.code(task)
-
-
 if __name__ == "__main__":
     main()
