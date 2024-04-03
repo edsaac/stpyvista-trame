a704cbeee71322a4a1db6c2cf3cc559a20f0fb8f
edsaa <esaavedrac@u.northwestern.edu>
2024-04-03 12:00:24 -0500

    backtrack changes to cloudflared

--- embed_trame.py
+++ embed_trame.py
@@ -1,105 +1,138 @@
-import pyvista as pv
-from pyvista.plotting import Plotter
+import streamlit as st
+import shlex
+from subprocess import Popen, check_output
+from subprocess import PIPE, STDOUT
+import atexit
+import os
+from pathlib import Path
+from collections import namedtuple
+from time import sleep
 
-import asyncio
-import streamlit as st 
-from streamlit.components.v1 import declare_component
-import nest_asyncio
-import threading
+CF_Connection = namedtuple("CF_Connection", ["process", "address"])
+PORT = 17007
 
-from trame.app import get_server
-from trame.ui.vuetify import SinglePageLayout
-from trame.widgets import vtk, client, trame
+IN_COMMUNITY_CLOUD = True
+PYCOMMAND = "/home/adminuser/venv/bin/python" if IN_COMMUNITY_CLOUD else "python"
+CONTROL_PANEL = True
 
-pv.OFF_SCREEN = True
-pv.start_xvfb()
-nest_asyncio.apply()
+CLOUDFLARED_PATH = Path("./cloudflared/cloudflared-linux-amd64")
+TRAME_APP_PATH = "./trame_example/grand_canyon.py"
 
-def basic_plotter():
-    plotter = pv.Plotter()
+st.set_page_config(initial_sidebar_state="collapsed")
 
-    ## Create a mesh with a cube
-    mesh = pv.Sphere(center=(0, 0, 0))
+if not Path("./cloudflared").exists():
+    os.system("mkdir cloudflared")
+    st.rerun()
 
-    ## Add some scalar field associated to the mesh
-    mesh["myscalar"] = mesh.points[:, 2] * mesh.points[:, 0]
-    mesh.set_active_scalars("myscalar")
+if not CLOUDFLARED_PATH.exists():
+    os.system(
+        f"wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O {CLOUDFLARED_PATH}"
+    )
+    os.system(f"chmod 777 {CLOUDFLARED_PATH}")
+    st.rerun()
 
-    ## Add mesh to the plotter
-    plotter.add_mesh(mesh, cmap="bwr", line_width=1, label="sphere")
-    plotter.add_title("My sphere", color="black", font_size=14)
 
-    ## Final touches
-    plotter.view_isometric()
-    plotter.background_color = "lightgray"
+@st.cache_resource
+def launch_cloudflared(dummy: str = "cloud"):
+    command = (
+        f"./cloudflared/cloudflared-linux-amd64 tunnel --url http://localhost:{PORT}"
+    )
+    args = shlex.split(command)
+    p = Popen(args, stdout=PIPE, stderr=PIPE, text=True)
 
-    return plotter
+    address = "not_found"
+    sleep(2)
 
-class TrameApp:
+    for _ in range(50):
+        line = p.stderr.readline()
+        print(line)
+        if "https" in line and ".trycloudflare.com" in line:
+            address = line.rpartition("INF")[-1].replace("|", "").strip()
+            break
 
-    def __init__(self, plotter:Plotter) -> None:
-        
-        self.server = get_server(client_type="vue2")
-        state, ctrl = self.server.state, self.server.controller  # noqa
+    return CF_Connection(p, address)
 
-        with SinglePageLayout(self.server, show_drawer=False, height="500px") as layout:
-        
-            ## Layout
-            with layout.content:
-                view = vtk.VtkLocalView(plotter.ren_win)
-                ctrl.view_update = view.update
 
-            ## Streamlit component configuration
-            js = "console.log('First message!');\n"
+@st.cache_resource
+def launch_trame(path_script: str):
+    command = f"""{PYCOMMAND} -u {path_script} --server --port {PORT} &"""
+    # os.system(command)
+    args = shlex.split(command)
+    p = Popen(args, stdout=PIPE, stderr=PIPE, text=True)
 
-            with open("./streamlit-trame-component-lib.js") as f:
-                js += f.read()
+    return p
 
-            ctrl.swap = client.JSEval(exec=js).exec
-            trame.ClientTriggers(mounted=ctrl.swap)
 
-    async def async_run(self):
-        
-        if not self.server.running:
-            self.cor = self.server.start(
-                port=3001,
-                open_browser=False,
-                thread=True,
-                show_connection_info=True,
-                exec_mode="coroutine",
-                disable_logging=True,
-                timeout=0,
-            )
-        
-            loop = asyncio.new_event_loop()
-            self.task = loop.create_task(self.cor, name="st_trame")
-            print("Create task running")
-            
-            loop.run_forever()  # This is blocking
+def close_all(p_trame: Popen, p_cloudflared: Popen):
+    close_trame(p_trame)
+    close_cloudflared(p_cloudflared)
+    st.rerun()
 
-        await self.server.ready
 
-    def run(self):
-        new_loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(new_loop)
-        new_loop.run_until_complete(self.async_run())
-        return
+def close_trame(p_trame: Popen):
+    print("Closing trame")
+    p_trame.terminate()
+    del st.session_state.trame_running
+
+
+def close_cloudflared(p_cloudflared: Popen):
+    print("Closing cloudflared")
+    p_cloudflared.terminate()
+    del st.session_state.cloudflared
+
 
 def main():
+    st.markdown(
+        """
+    <style>
+        div[data-testid="stAppViewBlockContainer"]{
+            padding-top: 1rem;
+        }
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
 
-    st_trame = declare_component("trame", url="http://localhost:3001")
-    st.title("streamlit-trame")
+    st.title("🧇 Trame within streamlit")
+
+    if "trame_running" not in st.session_state:
+        p_trame = launch_trame(TRAME_APP_PATH)
+        atexit.register(close_trame, p_trame)
+        st.session_state.trame_running = p_trame
+
+    if "cloudflared" not in st.session_state:
+        cloudflared = launch_cloudflared()
+        atexit.register(close_cloudflared, cloudflared.process)
+        st.session_state.cloudflared = cloudflared
+        st.rerun()
+
+    address = st.session_state.cloudflared.address
+    st.components.v1.iframe(address, height=580)
     
-    pl = basic_plotter()
-    print("Build plotter")
 
-    tmap = TrameApp(pl)
-    print("Get server and coroutine")
+    if CONTROL_PANEL:
+        with st.sidebar:
+            if (
+                st.text_input("Controls", type="password", label_visibility="collapsed")
+                == st.secrets.control_panel
+            ):
+                st.info(f"Running on {address}")
 
-    st_trame(key="my_trame")
-    print("Create component")    
+                if st.button("Reset cloudflared", on_click=st.cache_resource.clear):
+                    close_all(
+                        st.session_state.trame_running, st.session_state.cloudflared.process
+                    )
 
-    threading.Thread(target=tmap.run).start()
+                with st.form("Run commands"):
+                    quick_command = st.text_area("Command")
+                    run_command = st.form_submit_button("Run")
+
+                if run_command:
+                    task = check_output(
+                        ["bash", "-c", f"\"{quick_command}\""], text=True, stderr=STDOUT
+                    )
+                    st.code(task)
+
 
 if __name__ == "__main__":
     main()
---
+++ embed_trame_async.py
@@ -0,0 +1,105 @@
+import pyvista as pv
+from pyvista.plotting import Plotter
+
+import asyncio
+import streamlit as st 
+from streamlit.components.v1 import declare_component
+import nest_asyncio
+import threading
+
+from trame.app import get_server
+from trame.ui.vuetify import SinglePageLayout
+from trame.widgets import vtk, client, trame
+
+pv.OFF_SCREEN = True
+pv.start_xvfb()
+nest_asyncio.apply()
+
+def basic_plotter():
+    plotter = pv.Plotter()
+
+    ## Create a mesh with a cube
+    mesh = pv.Sphere(center=(0, 0, 0))
+
+    ## Add some scalar field associated to the mesh
+    mesh["myscalar"] = mesh.points[:, 2] * mesh.points[:, 0]
+    mesh.set_active_scalars("myscalar")
+
+    ## Add mesh to the plotter
+    plotter.add_mesh(mesh, cmap="bwr", line_width=1, label="sphere")
+    plotter.add_title("My sphere", color="black", font_size=14)
+
+    ## Final touches
+    plotter.view_isometric()
+    plotter.background_color = "lightgray"
+
+    return plotter
+
+class TrameApp:
+
+    def __init__(self, plotter:Plotter) -> None:
+        
+        self.server = get_server(client_type="vue2")
+        state, ctrl = self.server.state, self.server.controller  # noqa
+
+        with SinglePageLayout(self.server, show_drawer=False, height="500px") as layout:
+        
+            ## Layout
+            with layout.content:
+                view = vtk.VtkLocalView(plotter.ren_win)
+                ctrl.view_update = view.update
+
+            ## Streamlit component configuration
+            js = "console.log('First message!');\n"
+
+            with open("./streamlit-trame-component-lib.js") as f:
+                js += f.read()
+
+            ctrl.swap = client.JSEval(exec=js).exec
+            trame.ClientTriggers(mounted=ctrl.swap)
+
+    async def async_run(self):
+        
+        if not self.server.running:
+            self.cor = self.server.start(
+                port=3001,
+                open_browser=False,
+                thread=True,
+                show_connection_info=True,
+                exec_mode="coroutine",
+                disable_logging=True,
+                timeout=0,
+            )
+        
+            loop = asyncio.new_event_loop()
+            self.task = loop.create_task(self.cor, name="st_trame")
+            print("Create task running")
+            
+            loop.run_forever()  # This is blocking
+
+        await self.server.ready
+
+    def run(self):
+        new_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(new_loop)
+        new_loop.run_until_complete(self.async_run())
+        return
+
+def main():
+
+    st_trame = declare_component("trame", url="http://localhost:3001")
+    st.title("streamlit-trame")
+    
+    pl = basic_plotter()
+    print("Build plotter")
+
+    tmap = TrameApp(pl)
+    print("Get server and coroutine")
+
+    st_trame(key="my_trame")
+    print("Create component")    
+
+    threading.Thread(target=tmap.run).start()
+
+if __name__ == "__main__":
+    main()
